3a22f655a8954f1f0e7a079e48f5a6af60209198
Hamzeh <hamza_ali321@yahoo.com>
2022-09-09 17:30:11 -0700

    added css

--- app.js
+++ app.js
@@ -3,5 +3,3 @@ const button = document.getElementById('ask-button');
 
 const inputContainer = document.getElementById('inputs-container');
 const fortuneContainer = document.getElementById('fortune-container');
-
-button.addEventListener(type 'click'
