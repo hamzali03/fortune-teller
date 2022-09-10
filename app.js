f3e3afa20f28c68be5e70e6816d26d0f41ba24d5
Hamzeh <hamza_ali321@yahoo.com>
2022-09-09 17:40:10 -0700

    added css' git push git add -A git checkout -b dev ' '

--- app.js
+++
@@ -1,5 +0,0 @@
-const input = document.getElementById('question');
-const button = document.getElementById('ask-button');
-
-const inputContainer = document.getElementById('inputs-container');
-const fortuneContainer = document.getElementById('fortune-container');
